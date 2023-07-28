99d85d685c46c2633981e7fe4ff52b1295c442cd
Maulana Khoirusyifa <91378553+MaulK@users.noreply.github.com>
2023-07-28 10:54:48 +0700

    Update account.js

--- account.js
+++ account.js
@@ -1,34 +1,52 @@
-function loadAccountPage() {
-    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-    if (!currentUser) {
-        window.location.href = "login.html";
-        return;
-    }
+// Function to load user account data from local storage
+function loadUserData() {
+  return JSON.parse(localStorage.getItem("userData")) || {
+    points: 0,
+    autoClickerLevel: 0,
+    autoClickerCost: 10,
+    coins: 0,
+    rebirths: 0 // Initialize rebirths to 0 for new users
+  };
+}
 
-    document.getElementById("username").innerText = currentUser.username;
-    document.getElementById("points").innerText = currentUser.points;
-    document.getElementById("autoClickerLevel").innerText = currentUser.autoClickerLevel;
-    document.getElementById("autoClickerCost").innerText = currentUser.autoClickerCost;
+// Function to save user account data to local storage
+function saveUserData(userData) {
+  localStorage.setItem("userData", JSON.stringify(userData));
+}
+
+function loadAccountPage() {
+  const currentUser = loadUserData();
+  if (!currentUser) {
+    window.location.href = "login.html";
+    return;
+  }
+
+  document.getElementById("username").innerText = currentUser.username;
+  document.getElementById("points").innerText = currentUser.points;
+  document.getElementById("autoClickerLevel").innerText = currentUser.autoClickerLevel;
+  document.getElementById("autoClickerCost").innerText = currentUser.autoClickerCost;
+  document.getElementById("coins").innerText = currentUser.coins;
+  document.getElementById("rebirths").innerText = currentUser.rebirths;
 }
 
 function clickPoint() {
-    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-    if (currentUser) {
-        currentUser.points += 1;
-        localStorage.setItem("currentUser", JSON.stringify(currentUser));
-        loadAccountPage();
-    }
+  let userData = loadUserData();
+  if (userData) {
+    userData.points += 1;
+    saveUserData(userData);
+    loadAccountPage();
+  }
 }
 
 function buyAutoClicker() {
-    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-    if (currentUser && currentUser.points >= currentUser.autoClickerCost) {
-        currentUser.points -= currentUser.autoClickerCost;
-        currentUser.autoClickerLevel += 1;
-        currentUser.autoClickerCost *= 2;
-        localStorage.setItem("currentUser", JSON.stringify(currentUser));
-        loadAccountPage();
-    }
+  let userData = loadUserData();
+  if (userData && userData.points >= userData.autoClickerCost) {
+    userData.points -= userData.autoClickerCost;
+    userData.autoClickerLevel += 1;
+    userData.autoClickerCost *= 2;
+    saveUserData(userData);
+    loadAccountPage();
+  }
 }
 
 window.onload = loadAccountPage;
